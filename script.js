5b37a807d188851a0fcd8083ff213711a05a6ce9
Bell <210176980+robothello444@users.noreply.github.com>
2026-01-05 12:39:09 -0600

    Added clear button

--- script.js
+++ script.js
@@ -39,12 +39,17 @@ function operate(num1, num2, operator) {
 // Selectors
 const btnSelector = document.querySelectorAll("button.calc-button");
 const displayContainer = document.querySelector(".display");
-const equalBtnSelector = document.querySelector("button.equal-btn")
+const equalBtnSelector = document.querySelector("button.equal-btn");
+const clearBtn = document.querySelector(".btn-clear");
 
 
 btnSelector.forEach(button => {
     button.addEventListener('click', () => {
         if ((button.id < 9 || button.id) > 0 && operator === "") {
+            // Clears display if a operation was done before
+            if ((displayContainer.textContent !== "" && number1 == "" && number2 == "" && operator == "")) {
+            displayContainer.textContent = "";
+            }
             number1 += button.id;
             displayContainer.textContent += button.id;
             console.log(`First number: ${button.id}`) // Debug
@@ -68,6 +73,13 @@ equalBtnSelector.addEventListener('click', () => {
     operate(number1, number2, operator);
     number1 = ""; number2 = ""; operator = "";
 });
+
+clearBtn.addEventListener('click', () => {
+    displayContainer.textContent = "";
+    number1 = ""; number2 = ""; operator = "";
+    console.log(` Cleared ${number1}, ${number2}, ${operator}`) // Debug
+});
+
 
 // // Test (first run)
 // console.log(operate(number1, number2, operator)); // 10
